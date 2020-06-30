0ead360f26a7d526123777e0d3b16772a444d324
25Juan <1216181067@qq.com>
2020-06-30 22:15:40 +0800

    toast 功能做成

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,273 +1,160 @@
-import React, {
-  Component
-} from 'react';
-import PropTypes from 'prop-types';
-import {
-  ViewPropTypes,
-  StyleSheet,
-  View,
-  Text,
-  Animated,
-  Dimensions,
-  TouchableWithoutFeedback,
-  Easing,
-  Keyboard
-} from 'react-native';
-const TOAST_MAX_WIDTH = 0.8;
-const TOAST_ANIMATION_DURATION = 200;
+import React from "react" ;
+import {View, Text, StyleSheet, Animated, Dimensions,} from "react-native" ;
+import Component from "../Component";
+import PropTypes from "prop-types";
 
-const positions = {
-  TOP: 50,
-  BOTTOM: -50,
-  CENTER: 0
-};
-
-const durations = {
-  LONG: 3500,
-  SHORT: 2000
-};
-
-let styles = StyleSheet.create({
-  defaultStyle: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  containerStyle: {
-    padding: 10,
-    backgroundColor: '#000',
-    opacity: 0.8,
-    borderRadius: 5
-  },
-  shadowStyle: {
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 4,
-      height: 4
-    },
-    shadowOpacity: 0.8,
-    shadowRadius: 6,
-    elevation: 10
-  },
-  textStyle: {
-    fontSize: 16,
-    color: '#fff',
-    textAlign: 'center'
-  }
-});
-
-class Toast extends Component {
-  static propTypes = {
-    ...ViewPropTypes,
-    containerStyle: ViewPropTypes.style,
-    duration: PropTypes.number,
-    visible: PropTypes.bool,
-    position: PropTypes.number,
-    animation: PropTypes.bool,
-    shadow: PropTypes.bool,
-    keyboardAvoiding: PropTypes.bool,
-    backgroundColor: PropTypes.string,
-    opacity: PropTypes.number,
-    shadowColor: PropTypes.string,
-    textColor: PropTypes.string,
-    textStyle: Text.propTypes.style,
-    delay: PropTypes.number,
-    hideOnPress: PropTypes.bool,
-    onPress: PropTypes.func,
-    onHide: PropTypes.func,
-    onHidden: PropTypes.func,
-    onShow: PropTypes.func,
-    onShown: PropTypes.func
+/**
+ *
+ * @param {Object} props toast的属性
+ * @param {string} props.maskStyle 遮罩层的样式
+ * @param {number} props.boxStyle toast盒子的样式
+ * @param {number} props.textStyle 显示的文本的样式
+ * @param {function} props.visible 控制toast的显示
+ * @returns {*}
+ */
+export default class Toast extends Component {
+  static POSITION = {
+    TOP: 50,
+    CENTER: 0,
+    BOTTOM: -50
   };
+  static DURATION = {
+    LONG: 3500,
+    SHORT: 2000
+  } ;
 
+  static propTypes = {
+    visible: PropTypes.bool,
+    mask: PropTypes.bool,
+    position: PropTypes.number,
+    duration: PropTypes.number,
+  };
   static defaultProps = {
     visible: false,
-    duration: durations.SHORT,
-    animation: true,
-    shadow: true,
-    position: positions.BOTTOM,
-    opacity: 0.8,
-    delay: 0,
-    hideOnPress: true,
-    keyboardAvoiding: true
-  };
-
-  constructor() {
-    super(...arguments);
-    const window = Dimensions.get('window');
-    this.state = {
-      visible: this.props.visible,
-      opacity: new Animated.Value(0),
-      windowWidth: window.width,
-      windowHeight: window.height,
-      keyboardScreenY: window.height
-    };
+    mask: true,
+    position: Toast.POSITION.TOP,
+    duration: Toast.DURATION.SHORT
   }
 
-  componentDidMount = () => {
-    Dimensions.addEventListener('change', this._windowChanged);
-    if (this.props.keyboardAvoiding) {
-      Keyboard.addListener('keyboardDidChangeFrame', this._keyboardDidChangeFrame);
+  constructor(props) {
+    super(props)
+    this.state = {
+      opacity: new Animated.Value(0),
+      visible: props.visible,
+      mask: props.mask
     }
-    if (this.state.visible) {
-      this._showTimeout = setTimeout(() => this._show(), this.props.delay);
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if(this.state.visible){
+      this._show()
+    }else {
+      this._hide()
     }
-  };
+  }
 
-  componentDidUpdate = prevProps => {
-    if (this.props.visible !== prevProps.visible) {
-      if (this.props.visible) {
-        clearTimeout(this._showTimeout);
-        clearTimeout(this._hideTimeout);
-        this._showTimeout = setTimeout(() => this._show(), this.props.delay);
-      } else {
-        this._hide();
+  _show() {
+    if(this.timer) {
+      clearTimeout(this.timer) ;
+      this._hide(this._show)
+      return ;
+    }
+    Animated.timing(this.state.opacity, {
+      toValue: 1,
+      duration: 500
+    }).start(({finished}) => {
+      if (finished) {
+        this.setState({visible: true, opacity: new Animated.Value(1)})
+        this.timer = setTimeout(this._hide,this.props.duration)
       }
+    });
+  }
 
-      this.setState({
-        visible: this.props.visible
-      });
-    }
-  };
-
-  componentWillUnmount = () => {
-    this._hide();
-    Dimensions.removeEventListener('change', this._windowChanged);
-    Keyboard.removeListener('keyboardDidChangeFrame', this._keyboardDidChangeFrame);
-  };
-
-  _animating = false;
-  _root = null;
-  _hideTimeout = null;
-  _showTimeout = null;
-  _keyboardHeight = 0;
-
-  _windowChanged = ({ window }) => {
-    this.setState({
-      windowWidth: window.width,
-      windowHeight: window.height
-    })
-  };
-
-  _keyboardDidChangeFrame = ({ endCoordinates }) => {
-    this.setState({
-      keyboardScreenY: endCoordinates.screenY
-    })
-  };
-
-  _show = () => {
-    clearTimeout(this._showTimeout);
-    if (!this._animating) {
-      clearTimeout(this._hideTimeout);
-      this._animating = true;
-      this._root.setNativeProps({
-        pointerEvents: 'auto'
-      });
-      this.props.onShow && this.props.onShow(this.props.siblingManager);
-      Animated.timing(this.state.opacity, {
-        toValue: this.props.opacity,
-        duration: this.props.animation ? TOAST_ANIMATION_DURATION : 0,
-        easing: Easing.out(Easing.ease),
-        useNativeDriver: true
-      }).start(({finished}) => {
-        if (finished) {
-          this._animating = !finished;
-          this.props.onShown && this.props.onShown(this.props.siblingManager);
-          if (this.props.duration > 0) {
-            this._hideTimeout = setTimeout(() => this._hide(), this.props.duration);
-          }
-        }
-      });
-    }
-  };
-
-  _hide = () => {
-    clearTimeout(this._showTimeout);
-    clearTimeout(this._hideTimeout);
-    if (!this._animating) {
-      if (this._root) {
-        this._root.setNativeProps({
-          pointerEvents: 'none'
-        });
+  _hide = (callback) => {
+    Animated.timing(this.state.opacity, {
+      toValue: 0,
+      duration: 500
+    }).start(({finished}) => {
+      if (finished) {
+        this.setState({visible: false, opacity: new Animated.Value(0)},callback)
       }
+    });
+  }
 
-      if (this.props.onHide) {
-        this.props.onHide(this.props.siblingManager);
-      }
-
-      Animated.timing(this.state.opacity, {
-        toValue: 0,
-        duration: this.props.animation ? TOAST_ANIMATION_DURATION : 0,
-        easing: Easing.in(Easing.ease),
-        useNativeDriver: true
-      }).start(({finished}) => {
-        if (finished) {
-          this._animating = false;
-          this.props.onHidden && this.props.onHidden(this.props.siblingManager);
-        }
-      });
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.visible !== prevState.visible) {
+      return {visible: nextProps.visible, mask: nextProps.mask}
     }
-  };
+    return null
+  }
+
+  get maskStyle() {
+    if (this.props.position > 0) {
+      return {top: this.props.position}
+    } else if (this.props.position == 0) {
+      return {top: Dimensions.get('window').height / 2 - 30} // 大概居中，并不是真正意义上的居中
+    } else if (this.props.position < 0) {
+      return {bottom: Math.abs(this.props.position)}
+    }
+  }
+
+  get noMaskBox() {
+    if (this.props.position > 0) {
+      return {paddingTop: this.props.position}
+    } else if (this.props.position == 0) {
+      return {justifyContent: "center",} // 大概居中，并不是真正意义上的居中
+    } else if (this.props.position < 0) {
+      return {justifyContent: "flex-end", paddingBottom: Math.abs(this.props.position)}
+    }
+  }
 
   render() {
-    let {props} =  this;
-    const { windowWidth } = this.state;
-    let offset = props.position;
-
-    const { windowHeight, keyboardScreenY } = this.state;
-    const keyboardHeight = Math.max(windowHeight - keyboardScreenY, 0);
-    let position = offset ? {
-      [offset < 0 ? 'bottom' : 'top']: offset < 0 ? (keyboardHeight - offset) : offset
-    } : {
-      top: 0,
-      bottom: keyboardHeight
-    };
-
-    return (this.state.visible || this._animating) ? <View
-      style={[
-        styles.defaultStyle,
-        position
-      ]}
-      pointerEvents="box-none"
-    >
-      <TouchableWithoutFeedback
-        onPress={() => {
-          typeof this.props.onPress === 'function' ? this.props.onPress() : null
-          this.props.hideOnPress ? this._hide() : null
-        }}
-      >
-        <Animated.View
-          style={[
-            styles.containerStyle,
-            { marginHorizontal: windowWidth * ((1 - TOAST_MAX_WIDTH) / 2) },
-            props.containerStyle,
-            props.backgroundColor && {backgroundColor: props.backgroundColor},
-            {
-              opacity: this.state.opacity
-            },
-            props.shadow && styles.shadowStyle,
-            props.shadowColor && {shadowColor: props.shadowColor}
-          ]}
-          pointerEvents="none"
-          ref={ele => this._root = ele}
-        >
-          <Text style={[
-            styles.textStyle,
-            props.textStyle,
-            props.textColor && {color: props.textColor}
-          ]}>
-            {this.props.children}
-          </Text>
+    const props = this.props
+    const maskStyle = props.mask ? [Styles.maskBox, this.noMaskBox, props.maskStyle] : [Styles.noMaskBox, this.maskStyle]
+    if(!this.state.visible){
+      return null ;
+    }
+    console.log(this.state.visible)
+    return (
+      <View style={maskStyle}>
+        <Animated.View style={[Styles.box, props.boxStyle, {opacity: this.state.opacity}]}>
+          <Text style={[Styles.text, props.textStyle]}>hello world</Text>
         </Animated.View>
-      </TouchableWithoutFeedback>
-    </View> : null;
+      </View>)
   }
-}
 
-export default Toast;
-export {
-  positions,
-  durations
 }
+const Styles = StyleSheet.create({
+  maskBox: {
+    backgroundColor: 'red',
+    zIndex: 99,
+    display: "none",
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  noMaskBox: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    left: 0,
+    right: 0,
+    width: '100%',
+  },
+  box: {
+    backgroundColor: "rgba(0,0,0,0.8)",
+    borderRadius: 4,
+  },
+  text: {
+    color: "#fff",
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingLeft: 5,
+    paddingRight: 5,
+    fontSize: 11
+  }
+});
