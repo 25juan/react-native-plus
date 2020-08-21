ec0a126f9615454013770bf23903738acc4592a3
sgellar <1216181067@qq.com>
2020-08-21 10:05:04 +0800

    修改函数组件问题

--- src/components/Portal.js
+++ src/components/Portal.js
@@ -10,26 +10,34 @@ export let loading = null;
 export let actionSheet = null ;
 export let alert = null ;
 export let shareCard = null ;
-export default (props) => {
-  const _toast = createRef();
-  const _loading = createRef();
-  const _actionSheet = createRef() ;
-  const _alert = createRef() ;
-  const _shareCard = createRef() ;
-  useEffect(() => {
+const _toast = createRef();
+const _loading = createRef();
+const _actionSheet = createRef() ;
+const _alert = createRef() ;
+const _shareCard = createRef() ;
+/**
+ * 此处不能用函数式组件，用了函数式组件可能造成渲染问题
+ */
+export default class extends React.Component{
+  componentDidMount() {
     toast = _toast.current ;
     loading = _loading.current ;
     actionSheet = _actionSheet.current ;
     alert = _alert.current ;
     shareCard = _shareCard.current ;
-  },[]);
-  return (
-    <>
-      <Toast ref={_toast}/>
-      <Loading customActivityIndicator={props.customActivityIndicator || null} ref={_loading}/>
-      <ActionSheet ref={_actionSheet}/>
-      <Alert ref={_alert}/>
-      <ShareCard ref={_shareCard}/>
-    </>
-  )
+  }
+
+  render() {
+    const props = this.props ;
+    return (
+      <>
+        <Toast ref={_toast}/>
+        <Loading customActivityIndicator={props.customActivityIndicator || null} ref={_loading}/>
+        <ActionSheet ref={_actionSheet}/>
+        <Alert ref={_alert}/>
+        <ShareCard ref={_shareCard}/>
+        {props.children}
+      </>
+    )
+  }
 }
